e7f08b119fa9a445a558539be627fc20b4eece46
Angcroft <svenegas533@gmail.com>
2024-06-30 23:21:02 -0400

    Kinda finished exercise

---
+++ HeadTails/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace HeadTails {
+    class Program {
+        static void Main(string[] args) {
+            Random random = new Random();
+            string userInput;
+
+            do {
+                Console.WriteLine("Flipping the coin...");
+                string result = TossCoin(random);
+                Console.WriteLine($"The result is: {result}");
+
+                Console.WriteLine("¿Would you want to flip the coing again? (y/n)");
+                userInput = Console.ReadLine();
+
+                if(string.IsNullOrEmpty(userInput)) {
+                    userInput = "n";    //  In case of null entry
+                }
+
+            } while (userInput.ToLower() == "y");
+
+            Console.WriteLine("Thank you for using this program!");
+        }
+
+        static string TossCoin(Random number) {
+            int toss = number.Next(2);
+            return toss == 0 ? "Head" : "Tails";
+        }
+    }
+}
